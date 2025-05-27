bac65888ed6d4867246e34d5b2495ed36058b55d
unknown <zhangzhen>
2025-05-27 14:56:00 +0800

    youhua

--- Core/Src/uart_prot.c
+++ Core/Src/uart_prot.c
@@ -230,6 +230,7 @@ static void shutdown_func(uint8_t shutdown)//按键检测函数中调用
 
 void shutdown_func_from_button(void)
 {
+	extern uint8_t soc_onoff_state;
 	if(Key_Scan() == 1)
 	{
 		soc_onoff_state = !soc_onoff_state;
